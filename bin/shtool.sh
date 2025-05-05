d781316d54f62c8f71fc77b8d01ecb256806ee4b
Angel Rey <angel@monadical.com>
2025-05-05 17:46:20 -0500

    Refactor docker compose up

---
+++ bin/shtool.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SELFHOSTYOURTECH_ROOT="$( dirname "$SCRIPTS_ROOT")"
+APPS_ENABLED_FILE="$SELFHOSTYOURTECH_ROOT"/etc/apps-enabled.yaml
+
+tail_logs() {
+    local app="$1"
+    local APP_DIR="$SELFHOSTYOURTECH_ROOT/apps/$app"
+    cd "$APP_DIR"
+    docker compose logs -f --no-color 2>&1 | sed "s/^/[$app] /"
+}
+
+function run #description 'Run the docker compose stack'
+{
+    IFS=$'\n\t'        # Safer word splitting
+    DEBUG="false"
+
+    echo "Starting apps from managed-apps..."
+
+    APP_LIST=$(sed -n '/^apps:/,/^[^ ]/p' "$APPS_ENABLED_FILE" | grep '-' | sed -e 's/- //' -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+
+    # Deploy apps
+    for app in $APP_LIST; do
+        APP_DIR="$SELFHOSTYOURTECH_ROOT/apps/$app"
+        echo "Deploying $app in $APP_DIR..."
+
+        APP_DIR="$SELFHOSTYOURTECH_ROOT/apps/$app"
+
+        if [ ! -d "$APP_DIR" ]; then
+            echo "❌ Directory '$app' not found. Skipping."
+            exit 1
+        fi
+
+        (cd "$APP_DIR" && docker compose up -d) || {
+            echo "❌ Failed to deploy $app."
+            exit 1
+        }
+    done
+
+    if [[ "$DEBUG" == "true" ]]; then
+        echo "✅ All apps deployed. Tailing logs..."
+        # Stream logs for each app in background
+        for app in $APP_LIST; do
+            tail_logs "$app" &
+        done
+
+        wait  # Keep script running to stream logs
+    fi
+
+}
+
+function manage #description 'Manage stacks one by one'
+{
+    echo "$@"
+    cd "$SELFHOSTYOURTECH_ROOT"/apps/"$1"
+    shift
+    docker compose $@
+}
